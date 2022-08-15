61cc98baf7b294a49e6496079b075a3a53c765f6
nzhuangtao <1972449982@qq.com>
2022-08-15 17:42:13 +0800

    change yml

--- test/test.js
+++ test/test.js
@@ -3,6 +3,9 @@ const {sum,isSting} = require('../lib/index');
 test('1+2 = 3',()=>{
     expect(sum(1,2)).toBe(3);
 });
+test('1+3 = 3',()=>{
+    expect(sum(1,3)).toBe(4);
+});
 test('123 is not string',()=>{
     expect(isSting(123)).not.toBeTruthy();
 });
